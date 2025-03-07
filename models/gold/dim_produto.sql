bcf4e0883fc0888131a0886690a5fa7a8c2de0cf
Lucas Araujo <lucas.araujo@indicium.tech>
2025-03-07 20:10:47 +0000

    Criar yml do motivo_venda e ajustar documentaçcao

--- models/gold/dim_produto.sql
+++ models/gold/dim_produto.sql
@@ -1,11 +1,11 @@
 with
     dim_produto as (
         select 
-        {{ dbt_utils.generate_surrogate_key(['ID_PRODUTO']) }} as SK_PRODUTO
-        , DESCRICAO_PRODUTO
-        , CODIGO_PRODUTO
-        , DESCRICAO_SUBCATEGORIA
-        , DESCRICAO_CATEGORIA
+            {{ dbt_utils.generate_surrogate_key(['ID_PRODUTO']) }} as SK_PRODUTO
+            , DESCRICAO_PRODUTO
+            , CODIGO_PRODUTO
+            , DESCRICAO_SUBCATEGORIA
+            , DESCRICAO_CATEGORIA
         from {{ ref('silver__produto') }}
     )
 
